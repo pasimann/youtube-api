bdb8fc15859423ec586fc63d3b3136a859e4a628
Pasi Manninen <pasi.manninen@digia.com>
2017-04-05 10:47:52 +0300

    URL parameters.

--- node-app/app.js
+++ node-app/app.js
@@ -1,28 +1,24 @@
 var express = require('express')
 var request = require('request')
 
+var BASE_URL = 'http://localhost:8080'
+
 var app = express()
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/message', function (req, res, next) {
-  request('http://localhost:8080/generate-chat-message?receiver=Hello',
+app.get('/message/:receiver', function (req, res, next) {
+  var receiver = req.params.receiver
+  var query = `receiver=${receiver}`
+  request(`${BASE_URL}/generate-chat-message?${query}`,
     function (error, response, body) {
       if (error) {
         console.log('ERROR with user request!')
         return res.sendStatus(response.statusCode)
       }
       body = JSON.parse(body)
-
-      // console.log(body)
-      // var text
-      // for (var key in body) {
-      //   text += 'Index is: ' + key + '\n Description is:  ' + body[key]
-      // }
-      // console.log("Got a response: ", text)
-
       res.send(body.msg)
   })
 })
